aaab0d129dad0b5f83798d0961e4919d828334b7
Brandi Anderson <branderso87@gmail.com>
2017-07-03 17:47:59 -0500

    listening on port 3000 confirmed

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const express = require('express')
+const app = express()
 const mustache = require('mustache-express')
-const 
+const sequelize = require('sequelize')
+const session = require('session')
+const bodyParser = require('body-parser')
+
+app.engine('mustache', mustache())
+app.set('view engine', 'mustache')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.listen(3000, function () {
+  console.log("I'm listening")
+})
